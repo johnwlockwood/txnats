17748f79e74e0baa65835ba7bc78b3d452061ff7
John.Lockwood <johnwlockwood@gmail.com>
2016-11-06 22:31:23 -0800

    Make protocol required on actions

---
+++ tests/test_actions.py
@@ -0,0 +1,22 @@
+
+from twisted.trial import unittest
+
+import txnats
+from txnats import actions
+
+class IsNatsProtocolTest(unittest.TestCase):
+    maxDiff = None
+    def test_received_ping(self):
+        """
+        Ensure ReceivedPing can be instanciated.
+        """
+        actions.ReceivedPing(protocol=txnats.io.NatsProtocol())
+
+    def test_received_pong(self):
+        """
+        Ensure ReceivedPong takes the protocol.
+        """
+        actions.ReceivedPong(txnats.io.NatsProtocol())
+
+    def test_unsub(self):
+        actions.Unsub(4, txnats.io.NatsProtocol())
--- txnats/actions.py
+++ txnats/actions.py
@@ -7,45 +7,74 @@ from .validators import is_instance_of_nats_protocol
 
 @attr.s
 class ReceivedPing(object):
-    protocol = attr.ib(default=None, 
-        validator=attr.validators.optional(
-            is_instance_of_nats_protocol
-        )
-    )
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
+    
 
 @attr.s
 class ReceivedPong(object):
-    protocol = attr.ib(default=None, 
-        validator=attr.validators.optional(
-            is_instance_of_nats_protocol
-        )
-    )
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
+
 
 @attr.s
 class ReceivedMsg(object):
-    protocol = attr.ib(default=None, 
-        validator=attr.validators.optional(
-            is_instance_of_nats_protocol
-        )
-    )
-
     sid = attr.ib(validator=attr.validators.instance_of(int))
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
     subject = attr.ib(default="")
-    payload = attr.ib(
+    payload = attr.ib(default=None,
         validator=attr.validators.instance_of((bytes, str))
     )
     reply_to = attr.ib(default=None, 
         validator=attr.validators.optional(
-            attr.validators.instance_of((bytes, str, unicode)))
+            attr.validators.instance_of((bytes, str, unicode))
         )
     )
 
+
 @attr.s
 class ReceivedInfo(object):
-    protocol = attr.ib(default=None, 
-        validator=attr.validators.optional(
-            is_instance_of_nats_protocol
-        )
-    )
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
     server_info = attr.ib(default=None, 
         validator=attr.validators.instance_of(ServerInfo))
+
+
+@attr.s
+class Unsub(object):
+    sid = attr.ib(validator=attr.validators.instance_of(int))
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
+
+
+@attr.s
+class ReceivedMsg(object):
+    sid = attr.ib(validator=attr.validators.instance_of(int))
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
+    subject = attr.ib(default="")
+    queue_group = attr.ib(default=None, 
+        validator=attr.validators.optional(
+            attr.validators.instance_of((bytes, str, unicode))
+        )
+    )
+    on_msg = attr.ib(default=None)
+
+
+@attr.s
+class UnhandledCommand(object):
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
+    command = attr.ib(default=None)
+
+
+@attr.s
+class ConnectionLost(object):
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
+    reason = attr.ib(default=None)
+
+
+@attr.s
+class Disconnect(object):
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
+    reason = attr.ib(default=None)
+
+
+@attr.s
+class Connect(object):
+    protocol = attr.ib(validator=is_instance_of_nats_protocol)
+    client_info = attr.ib(default=None)
